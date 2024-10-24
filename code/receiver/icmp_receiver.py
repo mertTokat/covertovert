470c6e65ebe7e895d3823dfef576e08cc11a7f75
berkulutas <berkulutas2001@gmail.com>
2024-10-24 19:31:41 +0300

    receiver implemented

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,13 @@
-import scapy
+from scapy.all import *
 
-# Implement your ICMP receiver here
+def handle_packet(packet):
+    # Check if the packet is an ICMP request 
+    if packet.haslayer(ICMP) and packet[ICMP].type == 8:
+        packet.show()  # Print the ICMP request
+
+def capture_icmp():
+    sniff(filter="icmp", prn=handle_packet)
+
+if __name__ == "__main__":
+    capture_icmp()
+
